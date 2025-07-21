9efe8c1185140b790b89ef3aba5e3e1950e25ab8
jrealiquez-2021549 <jrealiquez-2021549@kinal.edu.gt>
2025-07-21 15:34:20 -0600

    feat: Se agrega controlador para entidades

---
+++ KinalToys-JavaApp/target/generated-sources/annotations/com/kinaltoys/dominio/Usuarios_.java
@@ -0,0 +1,17 @@
+package com.kinaltoys.dominio;
+
+import javax.annotation.processing.Generated;
+import javax.persistence.metamodel.SingularAttribute;
+import javax.persistence.metamodel.StaticMetamodel;
+
+@Generated(value="org.eclipse.persistence.internal.jpa.modelgen.CanonicalModelProcessor", date="2025-07-21T15:21:36", comments="EclipseLink-2.7.12.v20230209-rNA")
+@StaticMetamodel(Usuarios.class)
+public class Usuarios_ { 
+
+    public static volatile SingularAttribute<Usuarios, String> telefonoUsuario;
+    public static volatile SingularAttribute<Usuarios, String> direccionUsuario;
+    public static volatile SingularAttribute<Usuarios, String> apellidoUsuario;
+    public static volatile SingularAttribute<Usuarios, Integer> codigoUsuario;
+    public static volatile SingularAttribute<Usuarios, String> nombreUsuario;
+
+}
